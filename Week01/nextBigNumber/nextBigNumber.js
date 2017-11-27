a7e60464314a6fe6c6eaf247f1ebd146bc27e84d
SDuck4 <chaes4@naver.com>
2017-11-27 16:01:58 +0900

    Week01 nextBigNumber 해결

--- Week01/nextBigNumber/nextBigNumber.js
+++ Week01/nextBigNumber/nextBigNumber.js
@@ -7,10 +7,62 @@
     N이 주어질 때, N의 다음 큰 숫자를 찾는 nextBigNumber 함수를 완성하세요.
 */
 
-function nextBigNumber(n){
-	var answer = 0;
-
-	return answer;
+function nextBigNumber(n) {
+    // 입력값을 2진수 배열로
+    var arrNumber = n.toString(2).split('');
+    console.log(arrNumber.join(''));
+    // 입력값 배열의 1을 왼쪽으로 정렬
+    // : 1의 갯수는 유지한 채로 현재 자릿수에서 최대로 큰 숫자
+    var arrBiggest = arrNumber.slice(0).sort((a, b) => b - a);
+    // 입력값이 현재 자릿수에서 최대 값인 경우
+    if (arrEquals(arrNumber, arrBiggest)) {
+        // 자릿수 증가하고 첫번째 1을 제외한 모든 1을 오른쪽으로 정렬
+        // 첫번째 1을 제외한 1을 오른쪽으로 정렬
+        arrNumber = arrNumber.slice(1).sort();
+        // 자릿수 증가
+        arrNumber.unshift('0');
+        // 첫번째 1 추가
+        arrNumber.unshift('1');
+    }
+    // 입력값이 현재 자릿수에서 최대 값이 아닌 경우
+    else {
+        // 1의 숫자는 유지한채로 큰 숫자
+        // 마지막 01 패턴에서 10으로 교환, 그 뒷부분은 1을 오른쪽으로 정렬
+        var idx01 = 0;
+        for (var i = 0; i < arrNumber.length - 1; i++) {
+            // 마지막 01패턴 찾기
+            if (arrNumber[i] == '0' && arrNumber[i + 1] == '1') {
+                idx01 = i;
+            }
+        }
+        // 01 => 10 교환
+        arrExchange(arrNumber, idx01, idx01 + 1);
+        // 뒷부분 1을 오른쪽으로 정렬
+        arrNumber = arrNumber.slice(0, idx01 + 1).concat(arrNumber.slice(idx01 + 1).sort()); 
+    }
+    console.log(arrNumber.join(''));
+    // 10진수로 변환하여 반환
+    return parseInt(arrNumber.join(''), 2);
 }
 
-console.log(nextBigNumber(78));
+// 배열이 같음을 비교하는 함수
+function arrEquals(a, b) {
+    if (a.length == b.length) {
+        for (var i = 0; i < a.length; i++) {
+            if (a[i] !== b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+// 배열의 두 값을 교환하는 함수
+function arrExchange(arr, a, b) {
+    var temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+console.log(nextBigNumber(191956));
