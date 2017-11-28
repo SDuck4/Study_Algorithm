9d53cf9829521e78075f741458372e5334919d5c
SDuck4 <chaes4@naver.com>
2017-11-28 18:06:01 +0900

    jumpCase 메모이제이션 최적화

--- programmers.co.kr/level3/jumpCase/jumpCase.js
+++ programmers.co.kr/level3/jumpCase/jumpCase.js
@@ -13,29 +13,23 @@
     예를 들어 4가 입력된다면, 5를 반환해 주면 됩니다.
 */
 
-// 함수형 패러다임 사용 : 함수의 실행 결과를 저장해둔다
-var savedResult = {
+// 메모이제이션
+var memoryJumpCase = {
     '1': 1,
     '2': 2
 };
 
+// 멀리뛰기 경우의 수 함수
 function jumpCase(num) {
-    // 저장된 결과가 있으면
-    if (savedResult[num]) {
-        // 저장된 결과 반환
-        return savedResult[num];
-    }
     // 저장된 결과가 없으면
-    else {
-        // 재귀호출으로 결과 구함
-        var result = jumpCase(num - 1) + jumpCase(num - 2);
-        // 입력과 결과 저장
-        savedResult[num] = result;
-        // 결과 반환
-        return result;
+    if (memoryJumpCase[num] == undefined) {
+        // 재귀호출으로 결과 구해서 저장
+        memoryJumpCase[num] = jumpCase(num - 1) + jumpCase(num - 2);
     }
+    // 저장된 결과 반환
+    return memoryJumpCase[num];
 }
 
 console.log(jumpCase(100));
 
-console.log(savedResult);
+console.log(memoryJumpCase);
