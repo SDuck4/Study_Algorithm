b2b349d5da2a9733e2273b77cd82ad8df39ac6c0
SDuck4 <chaes4@naver.com>
2017-12-06 09:21:11 +0900

    bestSet 주석 추가

--- programmers.co.kr/level4/bestSet/bestSet.js
+++ programmers.co.kr/level4/bestSet/bestSet.js
@@ -10,17 +10,17 @@
 */
 
 function bestSet(n, s) {
-    if (n > s) {
+    if (n > s) {                            // n이 s보다 크면 불가능, 답이 자연수의 집합이기 때문.
         return [-1];
     }
-    var dividend = parseInt(s / n);
-    var remainder = s - dividend * n;
-    var answer = [];
+    var dividend = parseInt(s / n);         // s를 n으로 나눈 몫
+    var remainder = s - dividend * n;       // s를 n으로 나눈 나머지
+    var answer = [];                        // 정답 배열
     for (var i = 0; i < n; i++) {
         if (i < remainder) {
-            answer.unshift(dividend + 1);
+            answer.unshift(dividend + 1);   // 나머지 횟수는 1을 더해서 넣어주자
         } else {
-            answer.unshift(dividend);
+            answer.unshift(dividend);       // 몫을 정답 배열에 앞에 넣어주자, 앞에 넣어야 오름차순이됨
         }
     }
     return answer;
