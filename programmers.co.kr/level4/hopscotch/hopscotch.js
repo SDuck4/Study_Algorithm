3bd1fc495da201928d8f96390169484df5e46897
SDuck4 <chaes4@naver.com>
2017-12-07 15:16:34 +0900

    hopscotch 리팩토링 및 주석 정리

--- programmers.co.kr/level4/hopscotch/hopscotch.js
+++ programmers.co.kr/level4/hopscotch/hopscotch.js
@@ -16,43 +16,43 @@
 */
 
 function hopscotch(board, size) {
-    var maxScore = 0;
-    var doingSteps = [[]];
-    var doneSteps = [];
 
-    function hop(step) {
-        var lastCol = -1;
-        if (step.length > 0) {
-            lastCol = step[step.length - 1];
+    // BFS 탐색
+    var doingSteps = [[]];                              // 진행중인 스탭
+    var doneSteps = [];                                 // 완료된 스탭
+    function hop(step) {                                // 스탭 입력받아 다음 스탭으로 진행하는 함수
+        var lastCol = step[step.length - 1];            // 지난 스탭의 열
+        if (step.length == 0) {                         // 첫번째 스탭의 경우 예외처리
+            lastCol = -1;
         }
-        for (var i = 0; i < 4; i++) {
-            if (i != lastCol) {
-                var newStep = step.slice();
-                newStep.push(i);
-                doingSteps.push(newStep);
+        for (var i = 0; i < 4; i++) {                   // 0번 ~ 3번열 순회
+            if (i != lastCol) {                         // 지난 스탭의 열과 다르면
+                var newStep = step.slice();             // 지난 스탭 복사해서 새로운 스탭 생성
+                newStep.push(i);                        // 새로운 스탭 추가
+                doingSteps.push(newStep);               // 진행중인 스탭에 추가
             }
         }
     }
-
-    while (doingSteps.length > 0) {
-        var nowStep = doingSteps.shift();
-        if (nowStep.length == size) {
-            doneSteps.push(nowStep);
-        } else {
-            hop(nowStep);
+    while (doingSteps.length > 0) {                     // 진행중인 스탭이 없을 때 까지 실행
+        var nowStep = doingSteps.shift();               // 진행중인 스탭 꺼내옴
+        if (nowStep.length == size) {                   // 진행중인 스탭이 완료된 경우
+            doneSteps.push(nowStep);                    // 완료된 스탭으로 넣음
+        } else {                                        // 아직 진행중인 경우
+            hop(nowStep);                               // 다음 스탭으로 진행
         }
     }
-
-    for (var i = 0; i < doneSteps.length; i++) {
-        var doneStep = doneSteps[i];
-        var score = doneStep.reduce((p, c, i) => {
-            return p + board[i][c];
+    // 최고 점수 구하기
+    var maxScore = 0;                                   // 최고 점수
+    for (var i = 0; i < doneSteps.length; i++) {        // 완료된 스탭 순회
+        var doneStep = doneSteps[i];                    // 완료된 스탭
+        var score = doneStep.reduce((p, c, i) => {      // 완료된 스탭의 점수 계산
+            return p + board[i][c];                     // board 참조하며 점수 누적
         }, 0);
-        if (score > maxScore) {
-            maxScore = score;
+        if (score > maxScore) {                         // 최고 점수보다 완료된 스탭의 점수가 높으면
+            maxScore = score;                           // 최고 점수 갱신
         }
     }
-
+    // 최고 점수 반환
     return maxScore;
 }
 
