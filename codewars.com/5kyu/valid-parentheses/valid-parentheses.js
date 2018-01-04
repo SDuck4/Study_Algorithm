fa48324b08db6ceb5fac2d2e3dfafb2b7b9f8e75
SDuck <chaes4@naver.com>
2018-01-04 15:15:41 +0900

    valid-parantheses 문제 셋팅

---
+++ codewars.com/5kyu/valid-parentheses/valid-parentheses.js
@@ -0,0 +1,15 @@
+/*
+    https://www.codewars.com/kata/valid-parentheses
+
+    Write a function called that takes a string of parentheses, and determines if the order of the parentheses is valid.
+    The function should return true if the string is valid, and false if it's invalid.
+*/
+
+function validParentheses(parens){
+
+}
+
+console.log(validParentheses('()'), true);
+console.log(validParentheses(')(()))'), false);
+console.log(validParentheses('('), false);
+console.log(validParentheses('(())((()())())'), true);
