89a1a2f6300af32d151f44428ff2e189b291bcdf
SDuck <chaes4@naver.com>
2018-01-04 09:37:52 +0900

    sum-of-digits-slash-digital-root 문제 번역

--- codewars.com/6kyu/sum-of-digits-slash-digital-root/sum-of-digits-slash-digital-root.js
+++ codewars.com/6kyu/sum-of-digits-slash-digital-root/sum-of-digits-slash-digital-root.js
@@ -1,14 +1,14 @@
 /*
     https://www.codewars.com/kata/sum-of-digits-slash-digital-root
 
-    In this kata, you must create a digital root function.
+    이 카타에서 당신은 디지털 루트 함수를 만들어야합니다.
 
-    A digital root is the recursive sum of all the digits in a number.
-    Given n, take the sum of the digits of n.
-    If that value has two digits, continue reducing in this way until a single-digit number is produced.
-    This is only applicable to the natural numbers.
+    디지털 루트는 어떤 숫자의 모든 자릿수의 재귀적인 합입니다.
+    n이 주어지면 n의 모든 자릿수를 더하세요.
+    만약 그 합이 두 자릿수면, 한 자릿수가 될 때 까지 반복하세요.
+    이 과정은 오직 자연수에만 적용할 수 있습니다.
 
-    Here's how it works (Ruby example given):
+    아래는 어떻게 동작하는지에 대한 예입니다.(Ruby 예제):
 
     digital_root(16)
     => 1 + 6
