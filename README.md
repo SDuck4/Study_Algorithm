1404e2e09cc06280ffd76457409a086dac52d9b2
SDuck4 <cotmddn4@gmail.com>
2019-07-18 01:48:10 +0900

    Collatz 재풀이

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 알고리즘 스터디
 
+1. [NextIT 알고리즘 스터디](./NextIT_Study.md)
+
 ## 참고 사이트
 
 1. [프로그래머스](https://programmers.co.kr/)
---
+++ programmers.co.kr/level1/Collatz/Collatz.js
@@ -0,0 +1,14 @@
+function solution(num) {
+  let count = 0;                            // 작업 카운트
+  while (count < 500) {                     // 500번 반복하면 종료
+    if (num == 1) return count;             // 종료 조건
+    else if (num % 2 == 0) num = num / 2;   // 짝수일 때 작업
+    else num = num * 3 + 1;                 // 홀수일 때 작업
+    count += 1;                             // 작업 카운트 증가
+  }
+  return -1;                                // 500번 반복해도 1이 되지 않은 경우
+}
+
+console.log(solution(6));
+console.log(solution(16));
+console.log(solution(626331));
