4872d892ae8da049f982a4222d94cbc3e295dbd8
SDuck4 <cotmddn4@gmail.com>
2019-07-21 10:31:24 +0900

    LongPalindrome 문제 정리

--- programmers.co.kr/level3/LongPalindrome/LongPalindrome.js
+++ programmers.co.kr/level3/LongPalindrome/LongPalindrome.js
@@ -1,16 +1,3 @@
-/*
-  https://programmers.co.kr/learn/courses/30/lessons/12904
-
-  앞뒤를 뒤집어도 똑같은 문자열을 팰린드롬(palindrome)이라고 합니다.
-  문자열 s가 주어질 때, s의 부분문자열(Substring)중 가장 긴 팰린드롬의 길이를 return 하는 solution 함수를 완성해 주세요.
-
-  예를들면, 문자열 s가 abcdcba이면 7을 return하고 abacde이면 3을 return합니다.
-
-  제한사항
-  - 문자열 s의 길이 : 2500 이하의 자연수
-  - 문자열 s는 알파벳 소문자로만 구성
-*/
-
 function isPalindrome(str) {
   let result = false;
   let start = 0, end = str.length - 1;
@@ -41,6 +28,6 @@ function solution(str) {
   return answer;
 }
 
-console.log(solution('abcdcba'));
-console.log(solution('abacde'));
-console.log(solution('abcdefghijklmnop'));
+console.log(solution('abcdcba'));             // 7
+console.log(solution('abacde'));              // 3
+console.log(solution('abcdefghijklmnop'));    // 1
